41a37eafaa8ce9d8532a1221cf61c9f6ba4bfadc
Ilja <ilj.mur@gmail.com>
2024-07-04 10:11:28 +0300

    On application start, loading 100 city names to database and querying the current temperature for all 100 cities

--- src/main/java/com/meteo/citytemperature/service/CityService.java
+++ src/main/java/com/meteo/citytemperature/service/CityService.java
@@ -5,14 +5,17 @@ import com.meteo.citytemperature.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CityService {
     private static final String API_KEY = "9c4f1e01c060df147afcdeda9364b627";
     private static final String API_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=" + API_KEY + "&units=metric";
+    private static final String API_CITIES_URL = "https://restcountries.com/v3.1/all?fields=capital,unMember";
 
     @Autowired
     private CityRepository cityRepository;
@@ -23,7 +26,7 @@ public class CityService {
     }
 
     public void loadCities() {
-        String[] cities = {"Riga", "Tallinn", "Berlin"};
+        List<String> cities = getCapitalNames();
 
         for (String cityName : cities) {
             double temperature = getTemperatureForCity(cityName);
@@ -32,6 +35,28 @@ public class CityService {
             city.setTemperature(temperature);
             cityRepository.save(city);
         }
+    }
+
+    private List<String> getCapitalNames() {
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(API_CITIES_URL, String.class);
+
+        List<String> capitals = new ArrayList<>();
+        if (response != null) {
+            JSONArray countries = new JSONArray(response);
+            for (int i = 0; i < countries.length(); i++) {
+                JSONObject country = countries.getJSONObject(i);
+                JSONArray capitalArray = country.optJSONArray("capital");
+                Boolean isUnMember = (Boolean) country.get("unMember");
+                if (isUnMember && capitalArray != null && capitalArray.length() > 0) {
+                    capitals.add(capitalArray.getString(0));
+                }
+                if (capitals.size() >= 100) {
+                    break;
+                }
+            }
+        }
+        return capitals;
     }
 
     private double getTemperatureForCity(String cityName) {
